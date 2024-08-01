96ad96cfedbb6a4e62b52d3a4da917e7df587c17
thinhbo19 <111267094+thinhbo19@users.noreply.github.com>
2024-08-01 07:23:46 +0700

    a

--- controllers/home.js
+++ controllers/home.js
@@ -14,9 +14,9 @@ const HomeControls = {
         console.log(sender_psid);
 
         if (webhook_event.message) {
-          HomeControls.handleMessage(sender_psid, webhook_event.message);
+          handleMessage(sender_psid, webhook_event.message);
         } else if (webhook_event.postback) {
-          HomeControls.handlePostback(sender_psid, webhook_event.postback);
+          handlePostback(sender_psid, webhook_event.postback);
         }
       });
       res.status(200).send("EVENT_RECEIVED");
@@ -43,86 +43,86 @@ const HomeControls = {
       res.sendStatus(403);
     }
   }),
-
-  handleMessage: async (sender_psid, received_message) => {
-    let response;
-    if (received_message.text) {
-      response = {
-        text: `You sent the message: "${received_message.text}". Now send me an attachment!`,
-      };
-    } else if (received_message.attachments) {
-      let attachment_url = received_message.attachments[0].payload.url;
-      response = {
-        attachment: {
-          type: "template",
-          payload: {
-            template_type: "generic",
-            elements: [
-              {
-                title: "Is this the right picture?",
-                subtitle: "Tap a button to answer.",
-                image_url: attachment_url,
-                buttons: [
-                  {
-                    type: "postback",
-                    title: "Yes!",
-                    payload: "yes",
-                  },
-                  {
-                    type: "postback",
-                    title: "No!",
-                    payload: "no",
-                  },
-                ],
-              },
-            ],
-          },
-        },
-      };
-    }
-
-    await HomeControls.callSendAPI(sender_psid, response);
-  },
-
-  handlePostback: async (sender_psid, received_postback) => {
-    let response;
-
-    // Get the payload for the postback
-    let payload = received_postback.payload;
-
-    // Set the response based on the postback payload
-    if (payload === "yes") {
-      response = { text: "Thanks!" };
-    } else if (payload === "no") {
-      response = { text: "Oops, try sending another image." };
-    }
-    // Send the message to acknowledge the postback
-    HomeControls.callSendAPI(sender_psid, response);
-  },
-
-  callSendAPI: async (sender_psid, response) => {
-    let request_body = {
-      recipient: {
-        id: sender_psid,
-      },
-      message: response,
-    };
-    request(
-      {
-        uri: "https://graph.facebook.com/v2.6/me/messages",
-        qs: { access_token: process.env.PAGE_ACCESS_TOKEN },
-        method: "POST",
-        json: request_body,
-      },
-      (err, res, body) => {
-        if (!err) {
-          console.log("Message sent!");
-        } else {
-          console.error("Unable to send message:" + err);
-        }
-      }
-    );
-  },
 };
+
+function handleMessage(sender_psid, received_message) {
+  let response;
+  if (received_message.text) {
+    response = {
+      text: `You sent the message: "${received_message.text}". Now send me an attachment!`,
+    };
+  } else if (received_message.attachments) {
+    let attachment_url = received_message.attachments[0].payload.url;
+    response = {
+      attachment: {
+        type: "template",
+        payload: {
+          template_type: "generic",
+          elements: [
+            {
+              title: "Is this the right picture?",
+              subtitle: "Tap a button to answer.",
+              image_url: attachment_url,
+              buttons: [
+                {
+                  type: "postback",
+                  title: "Yes!",
+                  payload: "yes",
+                },
+                {
+                  type: "postback",
+                  title: "No!",
+                  payload: "no",
+                },
+              ],
+            },
+          ],
+        },
+      },
+    };
+  }
+
+  callSendAPI(sender_psid, response);
+}
+
+function handlePostback(sender_psid, received_postback) {
+  let response;
+
+  // Get the payload for the postback
+  let payload = received_postback.payload;
+
+  // Set the response based on the postback payload
+  if (payload === "yes") {
+    response = { text: "Thanks!" };
+  } else if (payload === "no") {
+    response = { text: "Oops, try sending another image." };
+  }
+  // Send the message to acknowledge the postback
+  callSendAPI(sender_psid, response);
+}
+
+function callSendAPI(sender_psid, response) {
+  let request_body = {
+    recipient: {
+      id: sender_psid,
+    },
+    message: response,
+  };
+  request(
+    {
+      uri: "https://graph.facebook.com/v2.6/me/messages",
+      qs: { access_token: process.env.PAGE_ACCESS_TOKEN },
+      method: "POST",
+      json: request_body,
+    },
+    (err, res, body) => {
+      if (!err) {
+        console.log("Message sent!");
+      } else {
+        console.error("Unable to send message:" + err);
+      }
+    }
+  );
+}
 
 export default HomeControls;
